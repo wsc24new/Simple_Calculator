3f6a3abaac73b0d89403f6b57148120cde4fc1e2
wusc <w.sc.2022@outlook.com>
2025-01-27 19:39:42 +0800

    V1.0.1

--- Calculator.cpp
+++ Calculator.cpp
@@ -7,30 +7,42 @@ int main() {
         system("chcp 65001>nul"); // 设置编码为UTF-8
     #endif
     cout << fixed << setprecision(2);
-    cout << "欢迎使用计算器！" << endl;
-    int num1, num2;
-    float _result;
+    cout << "欢迎使用四则计算器！" << endl;
+    double num1, num2;
+    double _result;
     bool calculate_flag = false;
 
     // 获取用户输入
     cout << "请输入第一个数字：" << endl;
-    if (!(cin >> num1)) {
-        cout << "输入错误！请输入一个有效的整数。" << endl;
-        return 1;
+    while (!(cin >> num1)) {
+        cin.clear();
+        while (cin.get()!= '\n') {
+            continue;
+        }
+        cout << "输入错误！请输入一个有效的数。" << endl;
+        cout << "请输入第一个数字：" << endl;
     }
     cout << "请输入第二个数字：" << endl;
-    if (!(cin >> num2)) {
-        cout << "输入错误！请输入一个有效的整数。" << endl;
-        return 1;
+    while (!(cin >> num2)) {
+        cin.clear();
+        while (cin.get() != '\n') {
+            continue;
+        }
+        cout << "输入错误！请输入一个有效的数。" << endl;
+        cout << "请输入第二个数字：" << endl;
     }
 
     // 选择运算符
     cout << "请选择运算符（1.加法，2.减法，3.乘法，4.除法）：" << endl;
     cout << "请选择：";
     int _operator;
-    if (!(cin >> _operator)) {
+    while (!(cin >> _operator)) {
+        cin.clear();
+        while (cin.get() != '\n') {
+            continue;
+        }
         cout << "输入错误！请输入一个有效的运算符编号。" << endl;
-        return 1;
+        cout << "请选择：";
     }
 
     // 进行运算
@@ -50,7 +62,7 @@ int main() {
                 calculate_flag = true;
                 break;
             }
-            _result = static_cast<float>(num1) / num2;
+            _result = num1 / num2;
             break;
         default:
             cout << "输入错误！请选择有效的运算符编号。" << endl;
